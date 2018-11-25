d5c3c3686c2f38438d7ca84d28f3c37935844a8d
Harry Fei <tiziyuanfang@gmail.com>
2018-11-25 15:07:02 +0800

    webrender: render basic text

--- rust_src/src/util.rs
+++ rust_src/src/util.rs
@@ -1,6 +1,7 @@
 //! Miscellaneous utility functions
 
 use crate::remacs_sys::EmacsInt;
+use webrender::api::units::*;
 
 #[no_mangle]
 pub extern "C" fn clip_to_bounds(lower: isize, num: EmacsInt, upper: isize) -> isize {
@@ -11,5 +12,27 @@ pub extern "C" fn clip_to_bounds(lower: isize, num: EmacsInt, upper: isize) -> i
         num
     } else {
         upper
+    }
+}
+
+pub trait HandyDandyRectBuilder {
+    fn to(&self, x2: i32, y2: i32) -> LayoutRect;
+    fn by(&self, w: i32, h: i32) -> LayoutRect;
+}
+// Allows doing `(x, y).to(x2, y2)` or `(x, y).by(width, height)` with i32
+// values to build a f32 LayoutRect
+impl HandyDandyRectBuilder for (i32, i32) {
+    fn to(&self, x2: i32, y2: i32) -> LayoutRect {
+        LayoutRect::new(
+            LayoutPoint::new(self.0 as f32, self.1 as f32),
+            LayoutSize::new((x2 - self.0) as f32, (y2 - self.1) as f32),
+        )
+    }
+
+    fn by(&self, w: i32, h: i32) -> LayoutRect {
+        LayoutRect::new(
+            LayoutPoint::new(self.0 as f32, self.1 as f32),
+            LayoutSize::new(w as f32, h as f32),
+        )
     }
 }

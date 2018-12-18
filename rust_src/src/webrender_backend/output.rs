1f032b8415321996934629062de71ce34dc45639
Harry Fei <tiziyuanfang@gmail.com>
2018-12-18 12:55:39 +0800

    webrender: implement more webrender backend functions * `x-display-color-cells` * `x-display-planes` * `x_wm_set_size_hint`

--- rust_src/src/webrender_backend/output.rs
+++ rust_src/src/webrender_backend/output.rs
@@ -204,6 +204,10 @@ impl Output {
 
         font_key
     }
+
+    pub fn get_color_bits(&self) -> u8 {
+        self.window.get_pixel_format().color_bits
+    }
 }
 
 pub type OutputRef = ExternalPtr<Output>;
